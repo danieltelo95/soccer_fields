a525289bc3a8ec588fb2d7dc07c75f2e4a96e6a9
jtello <fisk2190@gmail.com>
2024-03-04 02:19:44 -0500

    match settings

---
+++ app/lib/first_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MatchSettings extends StatefulWidget {
+  const MatchSettings({super.key});
+
+  @override
+  State<MatchSettings> createState() => _MatchSettingsState();
+}
+
+class _MatchSettingsState extends State<MatchSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(),
+      ),
+    );
+  }
+}
