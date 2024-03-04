be4cc3f6823e173a4582c745e92068d40b6de3f2
jtello <fisk2190@gmail.com>
2024-03-04 02:54:26 -0500

    text fields

--- app/lib/first_page.dart
+++ app/lib/first_page.dart
@@ -8,11 +8,44 @@ class MatchSettings extends StatefulWidget {
 }
 
 class _MatchSettingsState extends State<MatchSettings> {
+  final Map<String, TextEditingController> controllers = {
+    "Number of Players": TextEditingController(),
+    "Team A": TextEditingController(),
+    "Team B": TextEditingController(),
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: controllers["Number of Players"],
+                decoration: const InputDecoration(
+                    labelText: "Number of Players",
+                    hintText: "How many players?"),
+              ),
+              TextField(
+                controller: controllers["Team A"],
+                decoration: const InputDecoration(
+                  labelText: "Team A",
+                  hintText: "Name of team A",
+                ),
+              ),
+              TextField(
+                controller: controllers["Team B"],
+                decoration: const InputDecoration(
+                  labelText: "Team B",
+                  hintText: "Name of team B",
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
